71476ed62dc9b3ad59d098bf93769073639ec826
Alex <lovealex574@gmail.com>
2021-09-02 11:18:07 +0300

    Add class in timer (bug)

--- logic/timer.js
+++ logic/timer.js
@@ -4,19 +4,50 @@
  * @param {*} event 
  */
 import { minTimer, secTimer } from "./main.js";
+
 export function handleTime(event) {
     event.preventDefault();
     // event.target.setAttribute("style", "display: none");
     setInterval(
-        test,
+        startTimer,
         1000
     );
-    function test() {
-        if (secTimer.value <= 0) {
-            minTimer.value--;
-            secTimer.value = 59;
-        } else {
-            secTimer.value--;
-        }
+};
+
+function startTimer() {
+    if (minTimer.value <= 0 && secTimer.value <= 0) {
+        clearInterval(handleTime);
+    } else if (secTimer.value <= 0) {
+        minTimer.value--;
+        secTimer.value = 59;
+    } else {
+        secTimer.value--;
     }
-}
+};
+
+// class Timer {
+//     constructor(minTimer, secTimer) {
+//         this.minTimer = minTimer;
+//         this.secTimer = secTimer;
+//     }
+
+//     handleTime(event) {
+//         event.preventDefault();
+//         // event.target.setAttribute("style", "display: none");
+//         setInterval(
+//             startTimer,
+//             1000
+//         );
+//     };
+
+//     startTimer() {
+//         if (minTimer.value <= 0 && secTimer.value <= 0) {
+//             clearInterval(handleTime);
+//         } else if (secTimer.value <= 0) {
+//             minTimer.value--;
+//             secTimer.value = 59;
+//         } else {
+//             secTimer.value--;
+//         }
+//     };
+// }
