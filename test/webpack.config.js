22e7bebfcd1c7dc483911ccaec5b26cdab17e87b
lovealex574 <lovealex574@gmail.com>
2021-09-10 10:41:47 +0300

    add index.html in progect

--- test/webpack.config.js
+++ test/webpack.config.js
@@ -1,13 +1,10 @@
 const { resolve } = require('path');
 
-module.exports = {     
-    mode: 'development',             
-    entry: './js/main.js',         
+module.exports = {
+    mode: 'development',
+    entry: './js/main.js',
     output: {
         path: resolve(__dirname, 'build'),
-        filename: 'main.[contenthash].js',
-        devServer: {
-            port: 9000
-          }
+        filename: 'main.[contenthash].js'
     }
 };
